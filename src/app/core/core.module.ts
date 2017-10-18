900cfd3d35ee4cc8711b2f9206f8084cfda780cc
kkxujq <kkxujq@163.com>
2017-10-18 23:50:26 +0800

    init

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,14 @@
+import { NgModule, SkipSelf, Optional } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: []
+})
+export class CoreModule {  
+  constructor(@Optional() @SkipSelf() parent: CoreModule) {
+    if (parent) throw new Error('模块已经存在，不能再次加载！');
+  }
+}
